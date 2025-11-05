a5560d4884b3fa3e5ae011aadc4834e5899eaa62
praseedasuresh1992 <praseeeda@gmail.com>
2025-11-05 23:57:32 +0530

    .env added

--- src/api.jsx
+++ src/api.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
+
+const apiUrl=import.meta.env.VITE_API_URL
 const api=axios.create({
-    baseURL:"http://localhost:8000/",
-    withCredentials: true 
-   
+    baseURL:apiUrl ,
+    withCredentials:true
 })
+
 
 export default api
